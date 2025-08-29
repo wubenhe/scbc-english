f8342d21c850d48a994c1457852ab07138e00520
benwu23 <benwu23@cisco.com>
2025-08-29 10:59:34 -0700

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/southbay-church/' : '/',
+  base: mode === 'production' ? '/scbc-english/' : '/',
   server: {
     host: "::",
     port: 8080,
